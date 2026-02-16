dd28b764af66dee860386c5449a899845df7e2d5
Madhav Dixit <madhav@example.com>
2026-02-16 16:44:05 +0530

    16-02-2026 done

--- file.js
+++ file.js
@@ -10,22 +10,36 @@
 //     console.log('c');
 // };
 // a();
-const fetchData = new Promise((resolve, reject) =>{
-    const success = false;
+// const fetchData = new Promise((resolve, reject) =>{
+//     const success = false;
 
-    if(success){
-        resolve("Data fetched Successfully");
-        console.log("data fetched")
-    }else {
-        reject("No data found");
-        console.log("data is not fetched");
-    }
-});
+//     if(success){
+//         resolve("Data fetched Successfully");
+//         console.log("data fetched")
+//     }else {
+//         reject("No data found");
+//         console.log("data is not fetched");
+//     }
+// });
 
-fetchData
-    .then((message)=>{
-        console.log(message);
-    })
-    .catch((err)=>{
-    console.log(err);
-})
+// fetchData
+//     .then((message)=>{
+//         console.log(message);
+//     })
+//     .catch((err)=>{
+//     console.log(err);
+// })
+
+const sample = async ()=>{
+    console.log("Started");
+
+    await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(response => response.json())
+    .then((json) => console.log(json))
+
+    console.log("~ sample ~ Ended:");
+};
+
+console.log("~ sample ~ started---------1");
+sample();
+console.log("~ sample ~ started---------2");
